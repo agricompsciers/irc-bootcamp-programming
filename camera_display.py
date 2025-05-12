35ed0915a7404896ad453f3e6b6faf6b0f0fbf54
(A)nais <filzahalexa007@gmail.com>
2025-05-12 11:08:14 +0700

    Update camera_display.py

--- camera_display.py
+++ camera_display.py
@@ -36,7 +36,7 @@ while True:
         if face_alexa:
             cv2.putText(frame, "Filzah Alexandra", (20, 450), cv2.FONT_ITALIC, 2, (0, 255, 0), 3)
         else:
-            cv2.putText(frame, "Filzah Alexandra", (20, 450), cv2.FONT_ITALIC, 2, (255, 0, 0), 3)
+            cv2.putText(frame, " ", (20, 450), cv2.FONT_ITALIC, 2, (255, 0, 0), 3)
         
         cv2.imshow("video", frame)
 
