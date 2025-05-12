d27103d0cf5f4330fd734906e84cd384a54d1c1e
(A)nais <filzahalexa007@gmail.com>
2025-05-12 10:12:59 +0700

    Add files via upload

---
+++ led_servo.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+Servo servoku;
+int led1 = 13;
+int led2 = 12;
+int led3 = 11;
+
+void setup() {
+  servoku.attach(6);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  servoku.write(0);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  servoku.write(180);
+  delay(1000);
+}
